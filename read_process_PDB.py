94f40ace0c34395898ce052d3737055bf6a4e7c5
shivChitinous <shivamchitnis66@gmail.com>
2020-12-14 12:58:24 +0530

    Updates

--- read_process_PDB.py
+++ read_process_PDB.py
@@ -3,9 +3,9 @@
 #Basic script to parse atomic coordinates of PDB files by Chaitanya Athale, IISER Pune 2016/09/26
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.colors as cl
-import seaborn as sns
+import plotly.express as px
+from plotly.offline import iplot
+import plotly.graph_objects as go
 
 def atomize(pdbfile,heteroatom=False):
     pdbfile.seek(0) #setting readlines to 0
@@ -48,79 +48,42 @@ def atomize(pdbfile,heteroatom=False):
     atoms = atoms.sort_values(by=['serial'])
     return atoms
 
-def plot_protein(structure,skeleton=True,all_atoms=False,title="",chain_no=slice(None),point=None,sphere=None):
-    #get colours
-    structure['element-id'],indx = structure['element'].factorize()
-    clrs = sns.color_palette("Set1",np.size(indx))
-    cm = cl.ListedColormap(clrs.as_hex())
+def plot_protein(structure,title="",point=None,sphere=None):
+    mask = (structure['name']=='CA')|(structure['name']=='N')|(structure['name']=='C')
 
-    #define figure
-    fig = plt.figure(figsize=(4.5,4.5))
-    ax = fig.add_subplot(111, projection='3d')
-    plt.title(title)
-    
-    #plotting all atoms
-    if all_atoms:
-        ax.scatter3D(structure['x'], structure['y'], structure['z'],c=structure['element-id'], 
-                     marker='.', cmap=cm,alpha=0.4)
-    
-    #plotting skeleton
-    if skeleton:
-        slic = ((structure['name']=='CA')|(structure['name']=='N')|(structure['name']=='C'))
-        _,chains = structure['chain'].factorize()
-        chain_clr = sns.color_palette("Set2",np.size(chains))
+    fig1 = px.line_3d(structure.loc[mask],x='x',y='y',z='z',color='chain')
+    fig1.update_traces(line=dict(width=5),legendgroup="chain")
         
-        for i,p in enumerate(chains[chain_no]):
-            slicer = (slic&(structure['chain']==p))
 
-            #termini
-            ax.scatter3D(structure['x'][slicer].iloc[0], structure['y'][slicer].iloc[0], structure[slicer]['z'].iloc[0],
-                         color=clrs[0],s=50,marker='X')
-            ax.scatter3D(structure['x'][slicer].iloc[-1], structure['y'][slicer].iloc[-1], 
-                         structure[slicer]['z'].iloc[-1],color=clrs[1],s=50,marker='X')
+    fig2 = px.scatter_3d(structure,x='x',y='y',z='z',color='element',
+                        color_discrete_sequence=px.colors.qualitative.D3)
+    if sphere is None: fig2.update_traces(marker=dict(size=2),legendgroup="element",visible="legendonly")
+    else: fig2.update_traces(marker=dict(size=2),legendgroup="element")
 
-            #skeleton
-            ax.scatter3D(structure['x'].loc[slicer], structure['y'].loc[slicer], structure['z'].loc[slicer]
-                      ,s=5,color=chain_clr[i])
-            ax.plot3D(structure['x'].loc[slicer], structure['y'].loc[slicer], structure['z'].loc[slicer],
-                      alpha=0.5,linewidth=2,color=chain_clr[i])
+    fig2.add_traces(fig1.data)
+    fig2.update_layout(title=title,legend=dict(orientation="h",x=0,y=1,title=dict(text="Elements    Chain",side="top")))
     
     if point is not None:
-        ax.scatter3D(point[0], point[1], point[2],
-                      s=30,marker='o',color='k')
-    
+        fig3 = px.scatter_3d(pd.DataFrame(np.array([point]),columns=['x','y','z']),x='x',y='y',z='z',
+                    color_discrete_sequence=px.colors.qualitative.Dark2_r)
+        
+        fig3.update_traces(marker=dict(size=5))
+        fig2.add_traces(fig3.data)
+        
     if sphere is not None:
-        u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+        u, v = np.mgrid[0:2*np.pi:50j, 0:np.pi:50j]
         x = sphere*np.cos(u)*np.sin(v)+point[0]
         y = sphere*np.sin(u)*np.sin(v)+point[1]
         z = sphere*np.cos(v)+point[2]
-        ax.plot_surface(x, y, z, alpha=0.2, color='lightsteelblue')
+
+        fig4 = go.Figure(go.Surface(
+            x = x,
+            y = y,
+            z = z,
+            opacity=0.1
+            ))
         
-    ax.set_box_aspect((1,1,1))
-    set_axes_equal(ax)
+        fig4.update_traces(showscale=False)
+        fig2.add_traces(fig4.data)
 
-    plt.show()
-
-
-def set_axes_equal(ax: plt.Axes):
-    """Set 3D plot axes to equal scale.
-
-    Make axes of 3D plot have equal scale so that spheres appear as
-    spheres and cubes as cubes.  Required since `ax.axis('equal')`
-    and `ax.set_aspect('equal')` don't work on 3D.
-    """
-    limits = np.array([
-        ax.get_xlim3d(),
-        ax.get_ylim3d(),
-        ax.get_zlim3d(),
-    ])
-    origin = np.mean(limits, axis=1)
-    radius = 0.5 * np.max(np.abs(limits[:, 1] - limits[:, 0]))
-    set_axes_radius(ax, origin, radius)
-
-
-def set_axes_radius(ax, origin, radius):
-    x, y, z = origin
-    ax.set_xlim3d([x - radius, x + radius])
-    ax.set_ylim3d([y - radius, y + radius])
-    ax.set_zlim3d([z - radius, z + radius])
+    fig2.show()
